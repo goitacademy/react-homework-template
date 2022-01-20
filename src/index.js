db275db983abd6d43744bc6fa7a2457e1e1539d3
Alexander Repeta <alexander.repeta@gmail.com>
2022-01-20 13:28:23 +0200

    update jsconfig

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from '@components/App';
+import { App } from 'components/App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
