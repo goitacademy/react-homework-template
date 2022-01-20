77ea7cf2e0106bab3d0701bde44cb9e84c6818b7
Alexander Repeta <alexander.repeta@gmail.com>
2022-01-20 13:25:51 +0200

    add configs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,3 @@
 export const App = () => {
-  return <div>App</div>;
+  return <>React homework template</>;
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./components/App";
-import reportWebVitals from "./reportWebVitals";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from '@components/App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
