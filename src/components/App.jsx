f36af9ea064e1236f5797ca19d56960dcb81e24f
Alexander Repeta <alexander.repeta@gmail.com>
2022-01-31 15:20:55 +0200

    add first page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
 export const App = () => {
-  return <>React homework template</>;
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        textTransform: 'uppercase',
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
 };
