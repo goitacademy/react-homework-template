1ef84783d14b6b891dd353c295a050a1343f1805
Alexander Repeta <alexander.repeta@gmail.com>
2022-04-13 20:39:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,5 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
