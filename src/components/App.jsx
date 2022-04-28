85f4b6b60f39f37e24a48b35c999e444457da71a
Alexander Repeta <alexander.repeta@gmail.com>
2022-04-28 19:28:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,8 +7,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
+        color: '#010101'
       }}
     >
       React homework template
