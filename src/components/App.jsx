4dd84c45021e2c0c7bc2897087f76d496b2e5369
factvlad <fact1@ukr.net>
2022-08-10 22:42:06 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import user from 'data/user.json';
+import data from 'data/data.json';
 import Profile from 'components/Profile/';
+import Statistics from 'components/Statistics/';
 
 export const App = () => {
   return (
     <div>
       <Profile user={user} />
+      <Statistics title="Upload stats" stats={data}  />
+      {/* <Statistics stats={data}  /> */}
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,21 @@
+import s from './Statistics.module.css';
+
+const Statistics = props => {
+  const element = props.stats.map(item => (
+    <li key={item.id} className={s.item}>
+      <span className={s.label}>{item.label}</span>
+      <span className={s.percentage}>{item.percentage}</span>
+    </li>
+  ));
+
+  return (
+    <>
+      <section className={s.statistics}>
+        {props.title && <h2 className={s.title}>{props.title}</h2>}
+        <ul className={s.statList}>{element}</ul>
+      </section>
+    </>
+  );
+};
+
+export default Statistics;
